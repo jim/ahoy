313462338a41d575e9d9b8458f6ec54f9db3dd6a
Jim Benton <jim@autonomousmachine.com>
2021-11-15 14:24:31 -0600

    Initial implementation

---
+++ background.js
@@ -0,0 +1,11 @@
+function onClick (tab) {
+  browser.tabs.executeScript({
+    file: '/content_script.js'
+  }).then(function () {
+    browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
+      browser.tabs.sendMessage(tabs[0].id, { command: 'ahoy' });
+    });
+  }, console.error);
+}
+
+browser.browserAction.onClicked.addListener(onClick);
---
+++ content_script.js
@@ -0,0 +1,49 @@
+(function () {
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  if (window.__ahoyHasLoaded) {
+    return;
+  }
+  window.__ahoyHasLoaded = true;
+
+  function createAnchorLink (element) {
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', '#' + element.id);
+    anchor.classList.add('--ahoy-anchor');
+    anchor.appendChild(document.createTextNode('⚓️'));
+    element.prepend(anchor);
+  }
+
+  function addOrRemoveAnchors () {
+    if (window.__ahoyAnchorsAreAdded) {
+      removeAnchors();
+      window.__ahoyAnchorsAreAdded = false;
+    } else {
+      addAnchors();
+      window.__ahoyAnchorsAreAdded = true;
+    }
+  }
+
+  function addAnchors () {
+    const elements = document.querySelectorAll('[id]');
+    elements.forEach((element) => {
+      createAnchorLink(element);
+    });
+  }
+
+  function removeAnchors () {
+    document.querySelectorAll('.--ahoy-anchor').forEach(anchor => anchor.remove());
+  }
+
+  /**
+   * Listen for messages from the background script.
+   */
+  browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.command === 'ahoy') {
+      addOrRemoveAnchors();
+    }
+  });
+})();
